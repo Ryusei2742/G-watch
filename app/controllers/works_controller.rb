481c56dac38fcc6039f2ecba82d83457d54ac4fc
高田竜聖 <asd2f@asd.local>
2025-12-05 22:22:49 +0900

    Implement Works CRUD and basic views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,10 +1,11 @@
 class WorksController < ApplicationController
+  before_action :set_work, only: [:show, :edit, :update, :destroy]
+
   def index
-    @works = Work.all
+    @works = Work.order(created_at: :desc)
   end
 
   def show
-    @work = Work.find(params[:id])
     @reviews = @work.reviews.order(created_at: :desc)
 
     # 平均評価
@@ -17,7 +18,45 @@ class WorksController < ApplicationController
     @work = Work.new
   end
 
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to @work, notice: '作品を登録しました。'
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @work.update(work_params)
+      redirect_to @work, notice: '作品を更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path, notice: '作品を削除しました。'
+  end
+
+  private
+
+  def set_work
     @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params.require(:work).permit(
+      :title,
+      :description,
+      :year,
+      :series,
+      :recommended,
+      :thumbnail
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  # トップページ
   root to: 'home#index'
+
+  # ユーザー登録（signup）とログイン（login）
+  get    'signup', to: 'users#new'
+  post   'signup', to: 'users#create'
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  # 作品CRUD ＋ レビュー投稿
   resources :works do
     resources :reviews, only: [:create]
   end
