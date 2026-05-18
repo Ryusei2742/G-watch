e4fb51c57e9991d88dca9bb40bf88e46b91c7493
Ryusei2742 <asd2f@asd.local>
2026-05-18 22:07:57 +0900

    Fix duplicate password_digest migration

--- db/migrate/20251226154509_add_password_digest_to_users.rb
+++ db/migrate/20251226154509_add_password_digest_to_users.rb
@@ -1,5 +1,7 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[6.1]
   def change
+    return if column_exists?(:users, :password_digest)
+
     add_column :users, :password_digest, :string
   end
 end
